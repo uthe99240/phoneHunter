859c2e6762f892e9d4a05d5d4b256ff93aef3031
uthe99240 <uthe99240@gmail.com>
2022-03-02 20:54:15 +0600

    detail

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ const displaySearchResult = data => {
     // console.log(meals);
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    
+    const noResult = document.getElementById('no-result');
+    noResult.innerHTML='';
     if (data.length == 0) {
         // show no result found;
-        const noResult = document.getElementById('no-result');
+        
         const div = document.createElement('div')
         div.innerHTML = `<p class="card-text ">No Result Found !!!</p>`;
         noResult.appendChild(div);
@@ -34,11 +35,35 @@ const displaySearchResult = data => {
             <div class="card-body px-4">
                 <h5 class="card-title font-weight-bold">${data.phone_name}</h5>
                 <p class="card-text ">Brand : ${data.brand}</p>
-            <button class='btn btn-primary btn-lg btn-block' >Explore</button>
+            <button onclick="loadPhoneDetail('${data.slug}')" class='btn btn-primary btn-lg btn-block' >Explore</button>
             </div>
             
         </div>
         `;
         searchResult.appendChild(div);
+        noResult.innerHTML='';
     })
+}
+
+const loadPhoneDetail = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data));
+}
+
+const displayPhoneDetail = meal => {
+    console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+        <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    mealDetails.appendChild(div);
 }
